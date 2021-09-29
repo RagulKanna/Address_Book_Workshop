540b738e3c1164063abb3bf9af446576d73e52f0
RagulKanna <ragulragi619@gmail.com>
2021-09-29 16:56:21 +0530

    Added:Contact details are sorted in alphabetical order

---
+++ src/main/java/com/AddressBook/AddressBook.java
@@ -0,0 +1,11 @@
+package com.AddressBook;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AddressBook {
+
+    public static Map<String, Contact> book = new HashMap<String, Contact>();
+
+}
